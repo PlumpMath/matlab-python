038bde209d5195b06048b56ef2f8fdb5660d77ce
Kevin Matzen <kmatzen@cs.cornell.edu>
2012-07-20 21:21:42 -0400

    Cleaned up setup.py and it now will use the python install to find the correct numpy headers.  I still don't know how to get it to search for the MATLAB headers and libs.

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup, Extension
+import numpy as np
 
 interop = Extension('interop',
                     sources = ['interop.c'],
-										include_dirs = ['/hydra/S1/local/MATLAB/R2011a/extern/include',
-																		'/home/kmatzen/homebrew/Cellar/python3/3.2.3/lib/python3.2/site-packages/numpy/core/include'],
-										library_dirs = ['/hydra/S1/local/MATLAB/R2011a/bin/glnxa64'],
-										libraries = ['eng'])
+                    include_dirs = ['/hydra/S1/local/MATLAB/R2011a/extern/include',
+                                    np.get_include()],
+                    library_dirs = ['/hydra/S1/local/MATLAB/R2011a/bin/glnxa64'],
+                    libraries = ['eng'])
 
 setup (name = 'matlab',
        version = '1.0',
-       description = 'Does nothing more than provide bindings for the MATLAB engine C API',
+       description = 'Provides bindings for the MATLAB engine C API',
        ext_modules = [interop])
